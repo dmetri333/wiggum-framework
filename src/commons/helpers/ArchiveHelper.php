f9b262d97e0ec9d1f40bf82af7d91a9ed0038fd6
Demetri Mihalakakos <demetri@Demetris-MacBook-Pro.local>
2019-08-13 11:29:49 -0400

    added new helper Added new download options

---
+++ src/commons/helpers/ArchiveHelper.php
@@ -0,0 +1,71 @@
+<?php
+namespace wiggum\commons\helpers;
+
+use \ZipArchive;
+
+class ArchiveHelper
+{
+    
+    /**
+     * 
+     * @param string $filename
+     * @param array $files
+     * @throws \Exception
+     * @return boolean
+     */
+    public static function zip($filepath, array $files)
+    {
+        if (!class_exists('ZipArchive')) {
+            throw new \Exception('ArchiveHelper: ZipArchive is required php package');
+        }
+        
+        if (!is_dir(dirname($filepath))) {
+            return false;
+        }
+        
+        $zip = new ZipArchive();
+        $result = $zip->open($filepath, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+        if ($result !== true) {
+            return false;
+        }
+        
+        foreach ($files as $file) {
+            is_readable($file) && $zip->addFile($file, basename($file));
+        }
+        
+        return $zip->close();
+    }
+    
+    /**
+     * 
+     * @param string $source
+     * @param string $target
+     * @throws \Exception
+     * @return boolean
+     */
+    public static function unzip($source, $target)
+    {
+        if (!class_exists('ZipArchive')) {
+            throw new \Exception('ArchiveHelper: ZipArchive is required php package');
+        }
+        
+        if (!is_file($source)) {
+            return false;
+        }
+        
+        if (!is_dir($target)) {
+            return false;
+        }
+            
+        $zip = new ZipArchive();
+        $result = $zip->open($source);
+        if ($result !== true) {
+            return false;
+        }
+            
+        $zip->extractTo($target);
+        
+        return $zip->close();
+    }
+
+}
--- src/commons/helpers/DownloadHelper.php
+++ src/commons/helpers/DownloadHelper.php
@@ -10,23 +10,93 @@ class DownloadHelper
      * @param string $data
      * @param string $setMime
      */
-    public static function force($filename = '', $data = null, $setMime = false, $charset = 'UTF-8')
+    public static function forceFile($filename, $setMime = false, $charset = 'UTF-8')
     {
-        if ($filename === '' || $data === '') {
+        if ($filename === '') {
             return;
-        } else if ($data === null)  {
-            
-            $filepath = $filename;
-            $filename = explode('/', str_replace(DIRECTORY_SEPARATOR, '/', $filename));
-            $filename = end($filename);
-        
-            if (!@is_file($filepath) || ($filesize = @filesize($filepath)) === false) {
-                return;
-            }
-        
-        } else {
-            $filesize = strlen($data);
         }
+        
+        $filepath = $filename;
+        $filename = explode('/', str_replace(DIRECTORY_SEPARATOR, '/', $filename));
+        $filename = end($filename);
+        
+        if (!@is_file($filepath) || ($filesize = @filesize($filepath)) === false) {
+            return;
+        }
+        
+        // Set the default MIME type to send
+        $mime = 'application/octet-stream';
+        
+        $x = explode('.', $filename);
+        $extension = end($x);
+        
+        /*
+         //TODO fast way to look up mimes
+         if ($setMime) {
+         if (count($x) === 1 || $extension === '') {
+         return;
+         }
+         
+         // Load the mime types
+         $mimes =& get_mimes();
+         
+         // Only change the default MIME if we can find one
+         if (isset($mimes[$extension])) {
+         $mime = is_array($mimes[$extension]) ? $mimes[$extension][0] : $mimes[$extension];
+         }
+         }
+         */
+        
+        /* It was reported that browsers on Android 2.1 (and possibly older as well)
+         * need to have the filename extension upper-cased in order to be able to
+         * download it.
+         *
+         * Reference: http://digiblog.de/2011/04/19/android-and-the-download-file-headers/
+         */
+        if (count($x) !== 1 && isset($_SERVER['HTTP_USER_AGENT']) && preg_match('/Android\s(1|2\.[01])/', $_SERVER['HTTP_USER_AGENT'])) {
+            $x[count($x) - 1] = strtoupper($extension);
+            $filename = implode('.', $x);
+        }
+        
+        // Clean output buffer
+        if (ob_get_level() !== 0 && @ob_end_clean() === false) {
+            @ob_clean();
+        }
+        
+        // RFC 6266 allows for multibyte filenames, but only in UTF-8,
+        // so we have to make it conditional ...
+        $utf8Filename = ($charset !== 'UTF-8') ? self::convertToUtf8($filename, $charset) : $filename;
+        isset($utf8Filename[0]) && $utf8Filename = " filename*=UTF-8''".rawurlencode($utf8Filename);
+        
+        // Generate the server headers
+        header('Content-Type: '.$mime);
+        header('Content-Disposition: attachment; filename="'.$filename.'";'.$utf8Filename);
+        header('Expires: 0');
+        header('Content-Transfer-Encoding: binary');
+        header('Content-Length: '.$filesize);
+        header('Cache-Control: private, no-transform, no-store, must-revalidate');
+        
+        // Flush the file
+        if (@readfile($filepath) === false) {
+            return;
+        }
+        
+        exit;
+    }
+    
+    /**
+     *
+     * @param string $filename
+     * @param string $data
+     * @param string $setMime
+     */
+    public static function forceData($filename, $data, $setMime = false, $charset = 'UTF-8')
+    {
+        if ($filename === '' || empty($data)) {
+            return;
+        } 
+        
+        $filesize = strlen($data);
         
         // Set the default MIME type to send
         $mime = 'application/octet-stream';
@@ -80,15 +150,8 @@ class DownloadHelper
         header('Content-Length: '.$filesize);
         header('Cache-Control: private, no-transform, no-store, must-revalidate');
         
-        // If we have raw data - just dump it
-        if ($data !== null) {
-            exit($data);
-        }
-        
-        // Flush the file
-        if (@readfile($filepath) === false) {
-            return;
-        }
+        // raw data - just dump it
+        exit($data);
         
         exit;
     }
